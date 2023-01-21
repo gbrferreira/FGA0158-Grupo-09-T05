49d801a170b613fe64ba44b33c3985a67b9e9894
Gabriel Ferreira <gbrferreira02@gmail.com>
2023-01-21 01:44:37 -0300

    Modificaçao da classe Reserva e adicao da classe Lista Reserva

--- Development OO/user/Atendimento.java
+++ Development OO/user/Atendimento.java
@@ -1,7 +1,14 @@
 package user;
 
+import java.util.*;
+
 public class Atendimento extends Reserva {
     
+    public Atendimento(String d, int n, String h, String hf, int q) {
+        super(d, n, h, hf, q);
+        //TODO Auto-generated constructor stub
+    }
+
     public int calcHorarioRestante(){
         //a fazer
         return 5;
