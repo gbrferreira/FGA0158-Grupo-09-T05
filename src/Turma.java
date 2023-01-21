cf3cb480f224d21b2e0846206e1e7b3ef866e2a1
pedroMADBR <pedrommartins.2014@gmail.com>
2023-01-21 13:20:10 -0300

    organize files and create source and image directory

---
+++ src/Turma.java
@@ -0,0 +1,28 @@
+package user;
+
+public class Turma {
+    
+    private String disciplina;
+    private int numero;
+    private int vagas;
+    
+    public String getDisciplina() {
+        return disciplina;
+    }
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public int getVagas() {
+        return vagas;
+    }
+    public void setVagas(int vagas) {
+        this.vagas = vagas;
+    }
+
+}
