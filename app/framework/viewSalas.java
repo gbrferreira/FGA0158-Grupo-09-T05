9bb2e7cb73fead35debfda9baf2d42ae72c1d660
luanasoares0901 <202016720@aluno.unb.br>
2023-02-02 15:45:13 -0300

    update

--- app/framework/viewSalas.java
+++ app/framework/viewSalas.java
@@ -1,27 +1,4 @@
 package framework;
-import java.awt.*;
-import java.util.ArrayList;
-import javax.swing.*;
-import javax.swing.JScrollPane;
-import src.*;
 
-
-public class viewSalas extends JFrame {
-    public static void main (String []args){
-        new viewSalas();
-    }
-    public viewSalas() {
-        setTitle("Salas da FGA");
-        setSize(800, 600);
-        JPanel mainPanel = new JPanel(new GridBagLayout());
-        add(mainPanel);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        JLabel title = new JLabel("Salas FGA");
-        title.setFont(new Font("Arial", Font.BOLD, 40));
-        title.setHorizontalAlignment(JLabel.CENTER);
-
-
-    }
-
+public class viewSalas {
 }
